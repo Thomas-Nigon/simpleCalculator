7d47f097c2dc05e383caf0cbe56b0d13caf45893
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2023-03-07 18:19:19 +0100

    fix typo in HTML and replace console.log with alert in calc.js

--- calc.js
+++ calc.js
@@ -1,27 +1,25 @@
 // creation de la fonction calculator
 function calculator(){
-    // on demande une variable  firstValue a l'utilisateur   
+    // on demande  a l'utilisateur un premier chiffre, firstValue, pour notre calcul 
     let firstValue = prompt("first number:");
-    // on parseInt pour eviter une concatenation de string (2+2 22 a la place de 2+2=4).
-    
-    firstValue = parseInt(firstValue);          
-    
-    
-    
+    // on parseInt pour eviter une concatenation de string (2+2 22 a la place de 2+2=4).    
+    firstValue = parseInt(firstValue);    
+    // on demande l'operateur du calcul a realiser
     let operator= prompt("selector operator:")
+    //on demande un 2eme chiffre
     let secondValue = prompt("last value")
     secondValue = parseInt(secondValue)
 
     // commande switch pour effecter ce que l'utilisateur tape comme opérateur.
     switch (operator) {
-    case '+': console.log(firstValue + secondValue);
+    case '+': alert(firstValue + secondValue);
     break;
-    case '-': console.log(firstValue - secondValue);
+    case '-': alert(firstValue - secondValue);
     break;
-    case '*': console.log(firstValue * secondValue);
+    case '*': alert(firstValue * secondValue);
     break;
-    case '/': console.log(firstValue / secondValue);
+    case '/': alert(firstValue / secondValue);
     break;
     }
 }
-calculator();
+calculator()
